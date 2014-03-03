eb306738495ce2d5214b22db42cd3700c907455f
Andres Gutierrez <gutierrezandresfelipe@gmail.com>
2014-03-03 15:56:41 -0600

    Adding hooks controller

---
+++ app/controllers/HooksController.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+  +------------------------------------------------------------------------+
+  | Phosphorum                                                             |
+  +------------------------------------------------------------------------+
+  | Copyright (c) 2013-2014 Phalcon Team and contributors                  |
+  +------------------------------------------------------------------------+
+  | This source file is subject to the New BSD License that is bundled     |
+  | with this package in the file docs/LICENSE.txt.                        |
+  |                                                                        |
+  | If you did not receive a copy of the license and are unable to         |
+  | obtain it through the world-wide-web, please send an email             |
+  | to license@phalconphp.com so we can send you a copy immediately.       |
+  +------------------------------------------------------------------------+
+*/
+
+namespace Phosphorum\Controllers;
+
+use Phosphorum\Models\Users,
+	Phosphorum\Models\Posts,
+	Phosphorum\Models\PostsReplies,
+	Phalcon\Http\Response;
+
+class HooksController extends \Phalcon\Mvc\Controller
+{
+
+	public function initialize()
+	{
+		$this->view->disable();
+	}
+
+	public function mailReplyAction()
+	{
+		$response = new Response();
+		file_put_contents('../a.txt', print_r($_POST, true));
+		return $response;
+	}
+
+}
+
