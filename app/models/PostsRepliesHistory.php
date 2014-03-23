c394298ebbe2d8a1c2d11452e7aa39cce01ad72a
boston <bost56@gmail.com>
2014-03-24 00:34:55 +0600

    PostsRepliesHistory PSR fix

--- app/models/PostsRepliesHistory.php
+++ app/models/PostsRepliesHistory.php
@@ -19,28 +19,38 @@ namespace Phosphorum\Models;
 
 use Phalcon\Mvc\Model;
 
+/**
+ * Class PostsRepliesHistory
+ *
+ * @package Phosphorum\Models
+ */
 class PostsRepliesHistory extends Model
 {
 
-	public $id;
+    public $id;
 
-	public $posts_replies_id;
+    public $posts_replies_id;
 
-	public $users_id;
+    public $users_id;
 
-	public $content;
+    public $content;
 
-	public $created_at;
+    public $created_at;
 
-	public function beforeValidationOnCreate()
-	{
-		$this->created_at = time();
-	}
+    public function beforeValidationOnCreate()
+    {
+        $this->created_at = time();
+    }
 
-	public function initialize()
-	{
-		$this->belongsTo('posts_replies_id', 'Phosphorum\Models\PostsReplies', 'id', array(
-			'alias' => 'postReply'
-		));
-	}
+    public function initialize()
+    {
+        $this->belongsTo(
+            'posts_replies_id',
+            'Phosphorum\Models\PostsReplies',
+            'id',
+            array(
+                'alias' => 'postReply'
+            )
+        );
+    }
 }
