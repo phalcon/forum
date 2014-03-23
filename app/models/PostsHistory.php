94595001a280e8eefea1ab7fc0100cfc08739b1d
boston <bost56@gmail.com>
2014-03-24 00:30:53 +0600

    PostsBounties and PostsHistory PSR fix

--- app/models/PostsHistory.php
+++ app/models/PostsHistory.php
@@ -1,31 +1,57 @@
 <?php
 
+/*
+ +------------------------------------------------------------------------+
+ | Phosphorum                                                             |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2013-2014 Phalcon Team and contributors                  |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+
 namespace Phosphorum\Models;
 
 use Phalcon\Mvc\Model;
 
+/**
+ * Class PostsHistory
+ *
+ * @package Phosphorum\Models
+ */
 class PostsHistory extends Model
 {
 
-	public $id;
+    public $id;
 
-	public $posts_id;
+    public $posts_id;
 
-	public $users_id;
+    public $users_id;
 
-	public $content;
+    public $content;
 
-	public $created_at;
+    public $created_at;
 
-	public function beforeValidationOnCreate()
-	{
-		$this->created_at = time();
-	}
+    public function beforeValidationOnCreate()
+    {
+        $this->created_at = time();
+    }
 
-	public function initialize()
-	{
-		$this->belongsTo('posts_id', 'Phosphorum\Models\Posts', 'id', array(
-			'alias' => 'post'
-		));
-	}
+    public function initialize()
+    {
+        $this->belongsTo(
+            'posts_id',
+            'Phosphorum\Models\Posts',
+            'id',
+            array(
+                'alias' => 'post'
+            )
+        );
+    }
 }
