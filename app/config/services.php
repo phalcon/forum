94520afac52bccb2327d8b66b1442a847d13f5ef
boston <bost56@gmail.com>
2014-03-25 00:48:55 +0600

    services PSR

--- app/config/services.php
+++ app/config/services.php
@@ -32,119 +32,157 @@ use Ciconia\Ciconia;
 /**
  * The URL component is used to generate all kind of urls in the application
  */
-$di->set('url', function() use ($config) {
-	$url = new UrlResolver();
-	if (!$config->application->debug) {
-		$url->setBaseUri($config->application->production->baseUri);
-		$url->setStaticBaseUri($config->application->production->staticBaseUri);
-	} else {
-		$url->setBaseUri($config->application->development->baseUri);
-		$url->setStaticBaseUri($config->application->development->staticBaseUri);
-	}
-	return $url;
-}, true);
+$di->set(
+    'url',
+    function () use ($config) {
+        $url = new UrlResolver();
+        if (!$config->application->debug) {
+            $url->setBaseUri($config->application->production->baseUri);
+            $url->setStaticBaseUri($config->application->production->staticBaseUri);
+        } else {
+            $url->setBaseUri($config->application->development->baseUri);
+            $url->setStaticBaseUri($config->application->development->staticBaseUri);
+        }
+        return $url;
+    },
+    true
+);
 
 /**
  * Setting up volt
  */
-$di->set('volt', function($view, $di) use ($config) {
+$di->set(
+    'volt',
+    function ($view, $di) use ($config) {
 
-	$volt = new Volt($view, $di);
+        $volt = new Volt($view, $di);
 
-	$volt->setOptions(array(
-		"compiledPath"      => APP_PATH . "/app/cache/volt/",
-		"compiledSeparator" => "_",
-		"compileAlways"     => $config->application->debug
-	));
+        $volt->setOptions(
+            array(
+                "compiledPath"      => APP_PATH . "/app/cache/volt/",
+                "compiledSeparator" => "_",
+                "compileAlways"     => $config->application->debug
+            )
+        );
 
-	return $volt;
-}, true);
+        return $volt;
+    },
+    true
+);
 
 /**
  * Setting up the view component
  */
-$di->set('view', function() use ($config) {
+$di->set(
+    'view',
+    function () use ($config) {
 
-	$view = new View();
+        $view = new View();
 
-	$view->setViewsDir($config->application->viewsDir);
+        $view->setViewsDir($config->application->viewsDir);
 
-	$view->registerEngines(array(
-		".volt" => 'volt'
-	));
+        $view->registerEngines(
+            array(
+                ".volt" => 'volt'
+            )
+        );
 
-	return $view;
-}, true);
+        return $view;
+    },
+    true
+);
 
 /**
  * Database connection is created based in the parameters defined in the configuration file
  */
-$di->set('db', function() use ($config) {
+$di->set(
+    'db',
+    function () use ($config) {
 
-	$debug = $config->application->debug;
-	if ($debug) {
+        $connection = new DatabaseConnection($config->database->toArray());
 
-		$eventsManager = new EventsManager();
+        $debug = $config->application->debug;
+        if ($debug) {
 
-		$logger = new FileLogger(APP_PATH . "/app/logs/db.log");
+            $eventsManager = new EventsManager();
 
-		//Listen all the database events
-		$eventsManager->attach('db', function($event, $connection) use ($logger) {
-			if ($event->getType() == 'beforeQuery') {
-				$logger->log($connection->getSQLStatement(), \Phalcon\Logger::INFO);
-			}
-		});
-	}
+            $logger = new FileLogger(APP_PATH . "/app/logs/db.log");
 
-	$connection = new DatabaseConnection($config->database->toArray());
+            //Listen all the database events
+            $eventsManager->attach(
+                'db',
+                function ($event, $connection) use ($logger) {
+                    /** @var Phalcon\Events\Event $event */
+                    if ($event->getType() == 'beforeQuery') {
+                        /** @var DatabaseConnection $connection */
+                        $logger->log($connection->getSQLStatement(), \Phalcon\Logger::INFO);
+                    }
+                }
+            );
 
-	if ($debug) {
-		//Assign the eventsManager to the db adapter instance
-		$connection->setEventsManager($eventsManager);
-	}
+            //Assign the eventsManager to the db adapter instance
+            $connection->setEventsManager($eventsManager);
+        }
 
-	return $connection;
-});
+        return $connection;
+    }
+);
 
 /**
  * Queue to deliver e-mails in real-time
  */
-$di->set('queue', function() use ($config) {
-	return new Beanstalk(array(
-		'host' => $config->beanstalk->host
-	));
-}, true);
+$di->set(
+    'queue',
+    function () use ($config) {
+        return new Beanstalk(array(
+            'host' => $config->beanstalk->host
+        ));
+    },
+    true
+);
 
 /**
  * If the configuration specify the use of metadata adapter use it or use memory otherwise
  */
-$di->set('modelsMetadata', function() use ($config) {
+$di->set(
+    'modelsMetadata',
+    function () use ($config) {
 
-	if ($config->application->debug) {
-		return new MemoryMetaDataAdapter();
-	}
+        if ($config->application->debug) {
+            return new MemoryMetaDataAdapter();
+        }
 
-	return new MetaDataAdapter(array(
-		'metaDataDir' => APP_PATH . '/app/cache/metaData/'
-	));
+        return new MetaDataAdapter(array(
+            'metaDataDir' => APP_PATH . '/app/cache/metaData/'
+        ));
 
-}, true);
+    },
+    true
+);
 
 /**
  * Start the session the first time some component request the session service
  */
-$di->set('session', function() {
-	$session = new SessionAdapter();
-	$session->start();
-	return $session;
-}, true);
+$di->set(
+    'session',
+    function () {
+        $session = new SessionAdapter();
+        $session->start();
+        return $session;
+    },
+    true
+);
 
 /**
  * Router
  */
-$di->set('router', function() {
-	return include APP_PATH . "/app/config/routes.php";
-}, true);
+$di->set(
+    'router',
+    function () {
+        return include APP_PATH . "/app/config/routes.php";
+    },
+    true
+);
 
 /**
  * Register the configuration itself as a service
@@ -154,74 +192,92 @@ $di->set('config', $config);
 /**
  * Register the flash service with the Twitter Bootstrap classes
  */
-$di->set('flash', function() {
-	return new Phalcon\Flash\Direct(array(
-		'error' => 'alert alert-danger',
-		'success' => 'alert alert-success',
-		'notice' => 'alert alert-info',
-	));
-});
+$di->set(
+    'flash',
+    function () {
+        return new Phalcon\Flash\Direct(array(
+           'error'   => 'alert alert-danger',
+           'success' => 'alert alert-success',
+           'notice'  => 'alert alert-info',
+        ));
+    }
+);
 
 /**
  * Register the session flash service with the Twitter Bootstrap classes
  */
-$di->set('flashSession', function() {
-	return new Phalcon\Flash\Session(array(
-		'error' => 'alert alert-danger',
-		'success' => 'alert alert-success',
-		'notice' => 'alert alert-info',
-	));
-});
+$di->set(
+    'flashSession',
+    function () {
+        return new Phalcon\Flash\Session(array(
+            'error'   => 'alert alert-danger',
+            'success' => 'alert alert-success',
+            'notice'  => 'alert alert-info',
+        ));
+    }
+);
 
-$di->set('dispatcher', function() {
-	$dispatcher = new MvcDispatcher();
-	$dispatcher->setDefaultNamespace('Phosphorum\Controllers');
-	return $dispatcher;
-});
+$di->set(
+    'dispatcher',
+    function () {
+        $dispatcher = new MvcDispatcher();
+        $dispatcher->setDefaultNamespace('Phosphorum\Controllers');
+        return $dispatcher;
+    }
+);
 
 /**
  * View cache
  */
-$di->set('viewCache', function() use ($config) {
+$di->set(
+    'viewCache',
+    function () use ($config) {
 
-	if ($config->application->debug) {
-		$frontCache = new \Phalcon\Cache\Frontend\None();
-	} else {
-		//Cache data for one day by default
-		$frontCache = new \Phalcon\Cache\Frontend\Output(array(
-			"lifetime" => 86400 * 30
-		));
-	}
+        if ($config->application->debug) {
+            $frontCache = new \Phalcon\Cache\Frontend\None();
+        } else {
+            //Cache data for one day by default
+            $frontCache = new \Phalcon\Cache\Frontend\Output(array(
+                "lifetime" => 86400 * 30
+            ));
+        }
 
-	return new FileCache($frontCache, array(
-		"cacheDir" => APP_PATH . "/app/cache/views/",
-		"prefix"   => "forum-cache-"
-	));
-});
+        return new FileCache($frontCache, array(
+            "cacheDir" => APP_PATH . "/app/cache/views/",
+            "prefix"   => "forum-cache-"
+        ));
+    }
+);
 
 /**
  * Cache
  */
-$di->set('modelsCache', function() {
+$di->set(
+    'modelsCache',
+    function () {
 
-    //Cache data for one day by default
-    $frontCache = new \Phalcon\Cache\Frontend\Data(array(
-        "lifetime" => 86400 * 30
-    ));
+        //Cache data for one day by default
+        $frontCache = new \Phalcon\Cache\Frontend\Data(array(
+            "lifetime" => 86400 * 30
+        ));
 
-    return new \Phalcon\Cache\Backend\File($frontCache, array(
-        "cacheDir" => APP_PATH . "/app/cache/data/",
-        "prefix"   => "forum-cache-data-"
-    ));
-});
+        return new \Phalcon\Cache\Backend\File($frontCache, array(
+            "cacheDir" => APP_PATH . "/app/cache/data/",
+            "prefix"   => "forum-cache-data-"
+        ));
+    }
+);
 
-$di->set('markdown', function(){
-	$ciconia = new Ciconia();
-	$ciconia->addExtension(new \Phosphorum\Markdown\TableExtension());
-	$ciconia->addExtension(new \Phosphorum\Markdown\MentionExtension());
-	$ciconia->addExtension(new \Phosphorum\Markdown\BlockQuoteExtension());
-	$ciconia->addExtension(new \Ciconia\Extension\Gfm\FencedCodeBlockExtension());
-	$ciconia->addExtension(new \Ciconia\Extension\Gfm\UrlAutoLinkExtension());
-	return $ciconia;
-}, true);
-
+$di->set(
+    'markdown',
+    function () {
+        $ciconia = new Ciconia();
+        $ciconia->addExtension(new \Phosphorum\Markdown\TableExtension());
+        $ciconia->addExtension(new \Phosphorum\Markdown\MentionExtension());
+        $ciconia->addExtension(new \Phosphorum\Markdown\BlockQuoteExtension());
+        $ciconia->addExtension(new \Ciconia\Extension\Gfm\FencedCodeBlockExtension());
+        $ciconia->addExtension(new \Ciconia\Extension\Gfm\UrlAutoLinkExtension());
+        return $ciconia;
+    },
+    true
+);
