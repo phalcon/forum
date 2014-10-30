d65b57e39d69d4ab31874564adc769a85c7f3863
boxbillinggit <admin@boxbilling.com.pl>
2014-10-30 19:15:52 +0100

    missing Files

---
+++ app/models/TopicTracking.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ +------------------------------------------------------------------------+
+ | Phosphorum                                                             |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2013-2014 Phalcon Team and contributors                  |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+
+namespace Phosphorum\Models;
+
+use Phalcon\Mvc\Model;
+
+/**
+ * Class PostsHistory
+ *
+ * @property \Phosphorum\Models\Posts post
+ *
+ * @package Phosphorum\Models
+ */
+class TopicTracking extends Model
+{
+
+    public $id;
+
+    public $topic_id;
+
+    public $users_id;
+	
+	public function initialize()
+    {
+        $this->setSource('topic_tracking');
+    }
+	
+}
