4940cae9c5aeb40a453f368d8c9b3037c9a876b8
Serghei Iakovlev <sadhooklay@gmail.com>
2015-06-09 00:41:03 +0300

    Added Phalcon1 config

---
+++ tests/config/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+use \Phalcon\Mvc\Application;
+
+defined('APP_PATH') || define('APP_PATH', realpath('.'));
+
+$config = include __DIR__ . "/config.php";
+require APP_PATH . "/app/config/loader.php";
+include APP_PATH . "/tests/config/loader.php";
+
+$di = new \Phalcon\DI\FactoryDefault();
+
+require APP_PATH . "/app/config/services.php";
+require APP_PATH . "/vendor/autoload.php";
+
+$application = new Application;
+$application->setDI($di);
+
+return $application;
