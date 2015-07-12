879d91b32f9b1e237cc437c34e32f651c1d44062
Serghei Iakovlev <sadhooklay@gmail.com>
2015-07-12 15:16:08 +0300

    Added config for build

---
+++ tests/build/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ +------------------------------------------------------------------------+
+ | Phosphorum                                                             |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2013-2015 Phalcon Team and contributors                  |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+use \Phalcon\Mvc\Application;
+use Phalcon\DI\FactoryDefault;
+
+defined('APP_PATH') || define('APP_PATH', realpath('.'));
+
+require APP_PATH . "/vendor/autoload.php";
+
+$config = include APP_PATH . "/tests/config/config.php";
+require APP_PATH . "/tests/config/loader.php";
+
+$di = new FactoryDefault;
+
+require APP_PATH . "/tests/config/services.php";
+
+$application = new Application;
+$application->setDI($di);
+
+return $application;
---
+++ tests/build/loader.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ +------------------------------------------------------------------------+
+ | Phosphorum                                                             |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2013-2015 Phalcon Team and contributors                  |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
+$modelsDir      = $config->application->modelsDir;
+$controllersDir = $config->application->controllersDir;
+$libraryDir     = $config->application->libraryDir;
+
+$loader = new \Phalcon\Loader();
+
+// We're a registering a set of directories taken from the configuration file
+$loader->registerNamespaces(
+    [
+        'Phosphorum\Models'      => $modelsDir,
+        'Phosphorum\Controllers' => $controllersDir,
+        'Phosphorum'             => $libraryDir
+    ]
+);
+
+$loader->register();
