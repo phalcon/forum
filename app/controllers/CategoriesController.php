56ab1a5bc760b820ec2d3f4e2bd4defbc34ce312
Serghei Iakovlev <sadhooklay@gmail.com>
2015-07-12 17:26:02 +0300

    Cleanup and fixed CategoriesController

--- app/controllers/CategoriesController.php
+++ app/controllers/CategoriesController.php
@@ -4,7 +4,7 @@
  +------------------------------------------------------------------------+
  | Phosphorum                                                             |
  +------------------------------------------------------------------------+
- | Copyright (c) 2013-2014 Phalcon Team and contributors                  |
+ | Copyright (c) 2013-2015 Phalcon Team and contributors                  |
  +------------------------------------------------------------------------+
  | This source file is subject to the New BSD License that is bundled     |
  | with this package in the file docs/LICENSE.txt.                        |
@@ -28,49 +28,67 @@ use Phosphorum\Models\TopicTracking;
  */
 class CategoriesController extends ControllerBase
 {
-
     /**
      * @return \Phalcon\Http\ResponseInterface
      */
     public function indexAction()
     {
         $this->tag->setTitle('Forum');
-        $userId = $this->session->get('identity');
 
-        foreach (Categories::find() as $category) {
-            if (count(Posts::find("categories_id=".$category->id)) > 0) {
-                $last_author[$category->id] = $this
-                ->modelsManager
-                ->createBuilder()
-                ->from(array('p' => 'Phosphorum\Models\Posts'))
-                ->where('p.categories_id = "'.$category->id.'"')
-                ->join('Phosphorum\Models\Users', "u.id = p.users_id", 'u')
-                ->columns(array('p.users_id as users_id','u.name as name_user','p.title as post1_title','p.slug as post1_slug','p.id as post1_id'))
-                ->orderBy('p.id DESC')
-                ->limit(1)
-                ->getQuery()
-                ->execute();
+        $userId           = $this->session->get('identity');
+        $categories       = Categories::find();
+        $lastAuthor       = [];
+        $notRead          = [];
+        $postsPerCategory = [];
+
+        foreach ($categories as $category) {
+            /** @var \Phalcon\Mvc\Model\Resultset\Simple $posts */
+            $posts = Posts::find("categories_id=".$category->id);
+            $postsPerCategory[$category->id] = $posts->count();
+            if ($posts->count()) {
+                $lastAuthor[$category->id] = $this
+                    ->modelsManager
+                    ->createBuilder()
+                    ->from(['p' => 'Phosphorum\Models\Posts'])
+                    ->where('p.categories_id = "'.$category->id.'"')
+                    ->join('Phosphorum\Models\Users', "u.id = p.users_id", 'u')
+                    ->columns(['p.users_id as users_id','u.name as name_user','p.title as post1_title','p.slug as post1_slug','p.id as post1_id'])
+                    ->orderBy('p.id DESC')
+                    ->limit(1)
+                    ->getQuery()
+                    ->execute();
             } else {
-                $last_author[$category->id] = 0;
+                $postsPerCategory[$category->id] = 0;
+                $lastAuthor[$category->id] = 0;
             }
 
-          //SQL
-
-            $sql[$category->id] = "SELECT * FROM `posts` JOIN topic_tracking ON topic_tracking.topic_id WHERE concat(posts.id) AND NOT(FIND_IN_SET(posts.id, topic_tracking.topic_id)) AND categories_id = '{$category->id}' AND topic_tracking.user_id = '{$userId}'";
-            $not_read[$category->id] = $this->db->query($sql[$category->id]);
-
+            // SQL
+            $sql[$category->id] = "
+                SELECT *
+                FROM `posts` `p`
+                JOIN `topic_tracking` `tt` ON `tt`.`topic_id`
+                WHERE CONCAT(`p`.`id`)
+                  AND NOT(FIND_IN_SET(`p`.`id`, `tt`.`topic_id`))
+                  AND `p`.`categories_id` = '{$category->id}' AND `tt`.`user_id` = '{$userId}';
+            ";
+            $notRead[$category->id] = $this->db->query($sql[$category->id]);
         }
 
-        if ($userId !='') {
+        if ($userId) {
             $check_topic = new TopicTracking();
-            $check_topic->user_id = ''.$this->session->get('identity').'';
+            $check_topic->user_id = ''.$userId.'';
             $check_topic->topic_id = '9999999';
             $check_topic->create();
         }
 
-          $this->view->last_author = $last_author;
-          $this->view->not_read = $not_read;
-          $this->view->logged = $this->session->get('identity');
-          $this->view->categories = Categories::find();
+        $this->view->setVars(
+            [
+                'last_author'        => $lastAuthor,
+                'not_read'           => $notRead,
+                'logged'             => $userId,
+                'categories'         => $categories,
+                'posts_per_category' => $postsPerCategory
+            ]
+        );
     }
 }
