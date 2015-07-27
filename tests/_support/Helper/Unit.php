f3e0f489e978a41156172822d6d003c6f4bfda55
Serghei Iakovlev <sadhooklay@gmail.com>
2015-07-27 14:52:36 +0300

    Added Mockery support for Unit tests

--- tests/_support/Helper/Unit.php
+++ tests/_support/Helper/Unit.php
@@ -1,9 +1,29 @@
 <?php
+
 namespace Helper;
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
 
-class Unit extends \Codeception\Module
+use Codeception\Module;
+use Codeception\TestCase;
+use Mockery;
+
+/**
+ * Unit Helper
+ *
+ * Here you can define custom actions
+ * all public methods declared in helper class will be available in $I
+ *
+ * @package Helper
+ */
+class Unit extends Module
 {
-
+    /**
+     * Executed after each test
+     * HOOK: after suite
+     *
+     * @param TestCase $test
+     */
+    public function _after(TestCase $test)
+    {
+        Mockery::close();
+    }
 }
