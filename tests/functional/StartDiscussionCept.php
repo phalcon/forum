96c039d2b975ff6931a3d6579c6f0ca3c1e9ef95
Serghei Iakovlev <sadhooklay@gmail.com>
2015-07-29 10:02:22 +0300

    Fixed tests

--- tests/functional/StartDiscussionCept.php
+++ tests/functional/StartDiscussionCept.php
@@ -6,11 +6,11 @@
 $I = new Step\Functional\UserSteps($scenario);
 
 $I->wantTo('start a discussion');
-$I->amAdmin();
+
+$I->amRegularUser();
 $I->haveCategory(['name' => 'Testing', 'slug' => 'test', 'description' => 'codeception functional test']);
 $I->amOnPage('/');
 $I->see('Start a Discussion');
-$I->click('');
 $I->click('Start a Discussion');
 $I->see('Start a Discussion', 'h1');
 $I->seeRecord('Phosphorum\Models\Categories', ['name' => 'Testing']);
