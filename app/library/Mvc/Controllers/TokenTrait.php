2b1e265800b0a3c2b307f57ede6adbf8b4338f91
Serghei Iakovlev <sadhooklay@gmail.com>
2015-12-29 02:57:21 +0200

    Cleanup DiscussionsController

--- app/library/Mvc/Controllers/TokenTrait.php
+++ app/library/Mvc/Controllers/TokenTrait.php
@@ -48,4 +48,16 @@ trait TokenTrait
             $this->request->getQuery($csrfKey, null, 'dummy')
         );
     }
+
+    protected function checkTokenGet()
+    {
+        $csrfKey = $this->session->get('$PHALCON/CSRF/KEY$');
+        $csrfToken = $this->request->getQuery($csrfKey, null, 'dummy');
+
+        if (!$this->security->checkToken($csrfKey, $csrfToken)) {
+            $this->flashSession->error('Token error. This might be CSRF attack.');
+            return false;
+        }
+        return true;
+    }
 }
