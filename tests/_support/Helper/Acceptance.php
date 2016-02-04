7252eb57cd7797e91470d0f2891f30d6d54024fe
Serghei Iakovlev <sadhooklay@gmail.com>
2016-02-04 21:43:35 +0200

    Added sitemap test

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,9 +1,33 @@
 <?php
+
 namespace Helper;
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
 
-class Acceptance extends \Codeception\Module
+use Codeception\Module;
+
+/**
+ * Acceptance Helper
+ *
+ * Here you can define custom actions
+ * all public methods declared in helper class will be available in $I
+ *
+ * @package Helper
+ */
+class Acceptance extends Module
 {
+    /**
+     * Parse XML with Sitemap schema and return its URLs
+     *
+     * @param $string
+     * @return array
+     */
+    public function parseSitemap($string)
+    {
+        $urls = [];
+        $x = new \SimpleXMLElement($string);
+        foreach ($x->url as $n) {
+            $urls[] = $n->loc->__toString();
+        }
 
+        return $urls;
+    }
 }
---
+++ tests/acceptance/SitemapCept.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @var Codeception\Scenario $scenario
+ */
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('to make sure that every page in the sitemap');
+
+$I->sendGET('/sitemap');
+$I->seeResponseIsXml();
+$urls = $I->parseSitemap($I->grabResponse());
+
+foreach ($urls as $url) {
+    $I->amOnUrl($url);
+    $I->seeResponseCodeIs(200);
+}
