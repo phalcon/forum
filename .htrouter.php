c257fb490350f4698be04eea89b5e54304cefe3b
Serghei Iakovlev <sadhooklay@gmail.com>
2016-02-14 22:37:37 +0200

    Improved .htrouter.php

--- .htrouter.php
+++ .htrouter.php
@@ -1,6 +1,12 @@
 <?php
 
-if (!file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) {
-    $_GET['_url'] = $_SERVER['REQUEST_URI'];
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    return false;
 }
-return false;
+
+$_GET['_url'] = $_SERVER['REQUEST_URI'];
+require_once __DIR__ . '/public/index.php';
