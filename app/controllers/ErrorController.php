29f09a5579e26e8aa714b4b665e9d8e65d3a5874
Serghei Iakovlev <serghei@phalconphp.com>
2016-12-04 22:56:31 +0200

    Refactored error controller

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -20,7 +20,7 @@ namespace Phosphorum\Controllers;
 use Phalcon\Error\Error;
 
 /**
- * Error Controller
+ * Phosphorum\Controllers\Controller
  *
  * @package Phosphorum\Controllers
  */
@@ -33,71 +33,103 @@ class ErrorController extends ControllerBase
 
     public function initialize()
     {
-        /** @var \Phalcon\Error\Error $error */
-        $error = $this->dispatcher->getParam('error');
+        if ($this->dispatcher->hasParam('error')) {
+            $this->error = $this->dispatcher->getParam('error');
+        }
+
+        $this->view->setVars([
+            'debug'   => !environment(['production']),
+            'support' => container('config')->site->support
+        ]);
+    }
+
+    public function route400Action()
+    {
+        $this->createError(
+            'Bad request',
+            400,
+            'Something is not quite right.',
+            __LINE__
+        );
+    }
+
+    public function route401Action()
+    {
+        $this->response->setHeader('WWW-Authenticate', 'Digest realm="Access denied"');
+
+        $this->createError(
+            'Authorization required',
+            401,
+            'To access the requested resource requires authentication.',
+            __LINE__
+        );
+    }
+
+    public function route403Action()
+    {
+        $this->createError(
+            'Access is denied',
+            403,
+            'Access to this resource is denied by the administrator.',
+            __LINE__
+        );
+    }
+
+    public function route404Action()
+    {
+        $this->createError(
+            'Page not found',
+            404,
+            "Sorry! We can't seem to find the page you're looking for.",
+            __LINE__
+        );
+    }
+
+    public function route500Action()
+    {
+        $this->response->setHeader('Retry-After', 3600);
+
+        $this->createError(
+            'Something is not quite right',
+            500,
+            'We&rsquo;ll be back soon!',
+            __LINE__
+        );
+    }
+
+    public function route503Action()
+    {
+        $this->createError(
+            'Site Maintenance',
+            503,
+            'Unfortunately an unexpected system error occurred.',
+            __LINE__
+        );
+    }
+
+    protected function createError($title, $code, $message, $line)
+    {
+        $error = $this->error;
 
         if (!$error instanceof Error) {
             $error = new Error([
                 'type'        => -1,
-                'message'     => 'Something is not quite right',
+                'message'     => $title,
                 'file'        => __FILE__,
-                'line'        => __LINE__,
+                'line'        => $line,
                 'exception'   => null,
                 'isException' => false,
                 'isError'     => true,
             ]);
         }
 
-        $this->error = $error;
-
-        $this->view->setVars([
-            'error' => $this->error,
-            'debug' => environment(['development', 'testing']),
-        ]);
-    }
-
-    public function indexAction()
-    {
-        switch ($this->error->type()) {
-            case 404:
-                $this->tag->setTitle('Page not found');
-                $code = 404;
-                $message = 'Unfortunately, the page you are requesting can not be found!';
-                break;
-            case 403:
-                $this->tag->setTitle('Access is denied');
-                $code = 403;
-                $message = 'Access to this resource is denied by the administrator.';
-                break;
-            case 401:
-                $this->tag->setTitle('Authorization required');
-                $code = 401;
-                $message = 'To access the requested resource requires authentication.';
-                break;
-            default:
-                $this->tag->setTitle('Something is not quite right');
-                $code = 500;
-                $message = 'Unfortunately an unexpected system error occurred.';
-        }
-
-        $this->response->resetHeaders()->setStatusCode($code, null);
+        $this->tag->setTitle($title);
+        $this->response->setStatusCode($code);
 
         $this->view->setVars([
             'code'    => $code,
+            'error'   => $error,
             'message' => $message,
         ]);
-    }
-
-    public function route404Action()
-    {
-        $this->tag->setTitle('Page not found');
-        $code = 404;
-
-        $this->view->setVars([
-            'code'    => $code,
-            'message' => 'Unfortunately, the page you are requesting can not be found!',
-        ]);
-
-        $this->response->resetHeaders()->setStatusCode($code, 'Not Found');
     }
 }
