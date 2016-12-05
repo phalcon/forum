60ab283fbb33d8591d55df0476c356557d7977f4
Serghei Iakovlev <serghei@phalconphp.com>
2016-12-05 22:14:31 +0200

    Fixed memcached config

--- app/config/session.php
+++ app/config/session.php
@@ -15,9 +15,11 @@ return [
         'memcached' => [
             'adapter' => 'Libmemcached',
             'servers' => [
-                'host'   => env('MEMCACHED_HOST', '127.0.0.1'),
-                'port'   => env('MEMCACHED_PORT', 11211),
-                'weight' => env('MEMCACHED_WEIGHT', 100),
+                [
+                    'host'   => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port'   => env('MEMCACHED_PORT', 11211),
+                    'weight' => env('MEMCACHED_WEIGHT', 100),
+                ]
             ],
         ],
 
