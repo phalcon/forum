58fe20b6e4e92175492d067224a197ca0425feb7
Serghei Iakovlev <serghei@phalconphp.com>
2016-12-26 01:01:50 +0200

    Introduced Notifications Service

--- app/config/services.php
+++ app/config/services.php
@@ -1,11 +1,27 @@
 <?php
 
+/*
+ +------------------------------------------------------------------------+
+ | Phosphorum                                                             |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2013-2016 Phalcon Team and contributors                  |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file LICENSE.txt.                             |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to license@phalconphp.com so we can send you a copy immediately.       |
+ +------------------------------------------------------------------------+
+*/
+
 use Phosphorum\Model\Services\Service;
 
 return [
-    // abstract               => concrete
+    // abstract                  => concrete
     //
-    Service\Users::class      => Service\Users::class,
-    Service\Activities::class => Service\Activities::class,
-    Service\Posts::class      => Service\Posts::class,
+    Service\Users::class         => Service\Users::class,
+    Service\Activities::class    => Service\Activities::class,
+    Service\Posts::class         => Service\Posts::class,
+    Service\Notifications::class => Service\Notifications::class,
 ];
