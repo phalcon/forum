a03533b504cd4bdbf6faae8a084c615413e6daca
Serghei Iakovlev <serghei@phalconphp.com>
2018-01-20 21:53:55 +0200

    Cleaned tests

--- tests/acceptance/AssetsManagerCest.php
+++ tests/acceptance/AssetsManagerCest.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
-   +------------------------------------------------------------------------+
-   | Phosphorum                                                             |
-   +------------------------------------------------------------------------+
-   | Copyright (c) 2013-present Phalcon Team (https://www.phalconphp.com)   |
-   +------------------------------------------------------------------------+
-   | This source file is subject to the New BSD License that is bundled     |
-   | with this package in the file LICENSE.txt.                             |
-   |                                                                        |
-   | If you did not receive a copy of the license and are unable to         |
-   | obtain it through the world-wide-web, please send an email             |
-   | to license@phalconphp.com so we can send you a copy immediately.       |
-   +------------------------------------------------------------------------+
-   | Authors: Sergii Svyrydenko <sergey.v.sviridenko@gmail.com>             |
-   +------------------------------------------------------------------------+
- */
-
 use Helper\Post;
 use Helper\User;
 use Helper\Category;
@@ -56,7 +39,6 @@ class AssetsManagerCest
 
         $I->amOnPage("/discussion/{$postId}/test_assets");
         $I->seeInSource('Testing css and js collections');
-        $I->seeInSource('/assets/global');
         $I->seeInSource('/assets/global');
         $I->dontSeeInSource('/css/bootstrap.min.css');
         $I->seeFileFound('global.js','public/assets');
