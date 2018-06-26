d40d51859e3db790af7d3a8f6aaa5cb5b28e26ba
Serghei Iakovlev <serghei@phalconphp.com>
2018-06-26 09:07:04 +0300

    Cleaned up the Phosphorum\Core\Models\MetaData\Files

--- modules/Core/Models/MetaData/Files.php
+++ modules/Core/Models/MetaData/Files.php
@@ -18,11 +18,10 @@ declare(strict_types=1);
 
 namespace Phosphorum\Core\Models\MetaData;
 
-use Phalcon\Di;
 use Phalcon\Mvc\Model\MetaData\Files as BaseAdapter;
-use Phosphorum\Core\Environment;
 use Phosphorum\Core\Exceptions\DomainException;
 use Phosphorum\Core\Exceptions\InvalidArgumentException;
+use Phosphorum\Core\Traits\FileSystemTrait;
 
 /**
  * Phosphorum\Core\Models\MetaData\Files
@@ -31,10 +30,12 @@ use Phosphorum\Core\Exceptions\InvalidArgumentException;
  */
 class Files extends BaseAdapter
 {
+    use FileSystemTrait;
+
     /**
      * Files constructor.
      *
-     * @param array $options
+     * @param  array $options
      *
      * @throws InvalidArgumentException
      * @throws DomainException
@@ -42,49 +43,9 @@ class Files extends BaseAdapter
     public function __construct(array $options = [])
     {
         if (isset($options['metaDataDir'])) {
-            $options = $this->resolveMetaDataDir($options);
+            $options['metaDataDir'] = $this->resolveAbsolutePath($options['metaDataDir']);
         }
 
         parent::__construct($options);
-    }
-
-    /**
-     * Resolve MetaData path (make it absolute).
-     *
-     * @param  array $options
-     *
-     * @return array
-     *
-     * @throws InvalidArgumentException
-     * @throws DomainException
-     */
-    private function resolveMetaDataDir(array $options): array
-    {
-        $path = $options['metaDataDir'];
-
-        if (is_string($path) == false) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    'The metaDataDir parameter must be a string, got %s.',
-                    gettype($path)
-                )
-            );
-        }
-
-        if (ctype_print($path) == false) {
-            throw new DomainException(
-                'The metaDataDir can not have non-printable characters or be empty.'
-            );
-        }
-
-        $options['metaDataDir'] = rtrim($options['metaDataDir'], '\\/') . DIRECTORY_SEPARATOR;
-
-        // Looks like it is relative path
-        if ($path[0] !== DIRECTORY_SEPARATOR && preg_match('#\A[A-Z]:(?![^/\\\\])#i', $path) > 0) {
-            $env = Di::getDefault()->get(Environment::class);
-            $options['metaDataDir'] = $env->getPath($path);
-        }
-
-        return $options;
     }
 }
