2901fb67c381989faa1f1983be23c64c33767fda
Serghei Iakovlev <serghei@phalconphp.com>
2018-06-29 17:22:26 +0300

    Cleaned scripts.js

--- modules/Frontend/resources/assets/js/scripts.js
+++ modules/Frontend/resources/assets/js/scripts.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-
-//+++++++++++++++++++++++++++++++++++++++++++
-//     Jquery Code Start
-//+++++++++++++++++++++++++++++++++++++++++++     
-
-    $('.menuLinks a').click(function () {
+    $('.menuLinks a').on('click', function () {
         $('.menuLinks li').removeClass('active');
         $(this).parent("li").addClass('active');
+
         var tagid = $(this).data('tag');
+
         $('.forum-block').removeClass('activeBox').addClass('hideBox');
         $('#' + tagid).addClass('activeBox').removeClass('hideBox');
     });
-    $(".leftMenu").hide();
-    $('.mobail-bar').click(function () {
+
+    // $(".leftMenu").hide();
+
+    // TODO: There is no mobile-bar yet
+    $('.mobile-bar').on('click', function () {
         $('.leftMenu').slideToggle('');
     });
 });
